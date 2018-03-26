de60b3c59623b5d9f2ceef434004a3d8b2147787
nachomartinez30 <nahcomartinez3010@gmail.com>
2018-03-26 10:07:13 -0600

    agregando parrafo de ejemplo

--- proyecto_blog/admin/new_post.php
+++ proyecto_blog/admin/new_post.php
@@ -22,6 +22,14 @@ $sql = "";
         integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
         crossorigin="anonymous"></script>
 <h1 align="center">Nuevo Post</h1>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
 
 <form method="post">
     <div class="form-group">
